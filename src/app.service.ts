9d13d43c8f48835a4d662201572cb705168adec5
ilyongHwang <iyhwang@hnmcorp.kr>
2021-06-04 15:51:01 +0900

    Add: 가볍게 해봅시다

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World! Hello Friends! Hello! From teset 2!';
-  }
-}
