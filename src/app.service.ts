5a9a2e620bb2b299e128ff763e63d81db0e6c8ff
ilyongHwang <iyhwang@hnmcorp.kr>
2021-06-04 14:34:34 +0900

    test trigger

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Hello Friends!';
+    return 'Hello World! Hello Friends! Hello! From teset 2!';
   }
 }
