44f0a888e61aac94c6b515415424e80ca882961c
황일용 <iyhwang@hnmcorp.kr>
2021-06-07 17:54:19 +0900

    쓰러지기 일보직전

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World 1!!')
-})
+    res.send('Hello World !! Hello Friends!!\n');
+});
 
 app.listen(3000, () => {
     console.log('Server On!\n');
